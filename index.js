bd40a4d1b34b4fbd00af53bf3e9594a09f4dd1d4
Nikolai5991 <101453961+Nikolai5991@users.noreply.github.com>
2022-03-12 17:27:02 +0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,24 @@
+function Block (letter, Rate, father, code) {
+  this.letter = letter;
+  this.rate = rate;
+  this.used = used;
+  this.father= father;
+  this.code = code;
+}
+Function FindCode(block) {
+ If (tree [block.father].code != ‘ ‘) {
+  block.code = tree[block.father]. code + ‘ 1‘;
+ }
+  else {
+  if (block/letter == tree[minIndex].letter) {
+   block.code = ‘0‘ ;
+  }
+     else if (block.letter == tree[preminIndex].letter) {
+    block.code = ‘1‘ ;
+  }
+     else {
+    FindCode(tree[block.father]);
+    block.code = tree[block.father].code + ‘0’;
+  }
+ }
+}
